ad5559e76aaa1d02cf1f3e0ee0547b9ca7bad6a7
anna-zhyvka <annazhyvka516@gmail.com>
2023-05-01 21:26:34 +0300

    Initial commit

---
+++ js1.js
@@ -0,0 +1,33 @@
+let a = 'hello'
+let b = 'owu'
+let c = 'com'
+let d = 'ua'
+let e = 1
+let f = 10
+let g = -999
+let h = 123
+let x = 3.14
+let n = 2.7
+let l = 16
+let m = true
+let k = false
+console.log(a, b, c, d, e, f, g, h, x, n, l, m, k);
+
+
+let firstName = 'Анна'
+let middleName = 'Степанівна'
+let lastName = 'Живка'
+
+let pib = firstName + ' ' + middleName + ' ' + lastName
+console.log(pib);
+
+let name = prompt('Enter your first name:' )
+console.log(name);
+let secondName = prompt( 'Enter your middle name:')
+console.log(secondName);
+let age = prompt( 'Enter your age:')
+console.log(age);
+
+
+
+
